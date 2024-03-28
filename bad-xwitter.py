35ce2599a9f0d834d7d230e8b48dff7407661689
vaishnavi pandey <36892963+vishu1089@users.noreply.github.com>
2024-03-28 18:28:10 +0530

    Update bad-xwitter.py

--- bad-xwitter.py
+++ bad-xwitter.py
@@ -1,25 +1,23 @@
 import tweepy
+import ssl
 
 # Set up your Twitter API credentials
-consumer_key = "your_consumer_key"
-consumer_secret = "your_consumer_secret"
-access_token = "your_access_token"
-access_token_secret = "your_access_token_secret"
-
-# these above details you will get from developer account of twitter space
-
+consumer_key = "consumer key"
+consumer_secret = "consumer secret"
+access_token = "access token"
+access_token_secret = "access token secret"
+# bearer token AAAAAAAAAAAAAAAAAAAAAKkptAEAAAAAclZBKydyFUWosazXNZj4rvfmisU%3DytytFDQjDRY1gqA0PaarPzwpOr1P33D11EgqTb2Sp61OoWF17h
 # Authenticate with Twitter
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+context = ssl._create_unverified_context()
 # Create an API object
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 def unfollow_non_followers():
     try:
         # Get your followers and friends (people you follow)
-        followers = set(api.followers_ids())
-        friends = set(api.friends_ids())
+        followers = set(api.get_follower_ids())
+        friends = set(api.get_friend_ids())
 
         # Find users you follow but who don't follow you back
         non_followers = friends - followers
@@ -30,8 +28,12 @@ def unfollow_non_followers():
             print(f"Unfollowed user with ID {user_id}")
 
         print("Unfollow process completed successfully!")
-    except tweepy.TweepError as e:
+    except Exception as e:
         print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
+    import requests
+
+    # response = requests.get("https://api.twitter.com/1.1/followers/ids.json", verify=False) to disabe the ssl
+
     unfollow_non_followers()
